49c00b3c7e51b440d5777ddf32b2a8db79f2e22a
winguru <github@geoffthornton.me>
2019-05-17 14:33:09 -0400

    Changed version to 1.14.1 and separated buildme.sh into stages

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,4 @@
-MC_VERSION=1.13.2
+MC_VERSION=1.14.1
 export MC_VERSION
 BUILD_DIR=build
 TARGET_DIR=target
@@ -27,29 +27,48 @@ run_builder() {
 }
 
 make_spigot_server() {
-	docker build ${TARGET_DIR} --build-arg MC_VERSION -t winguru/minecraft-spigot:${MC_VERSION}
-	docker tag winguru/minecraft-spigot:${MC_VERSION} winguru/minecraft-spigot:latest
+	docker build ${TARGET_DIR} --build-arg MC_VERSION \
+		-t winguru/minecraft-spigot:${MC_VERSION} \
+		-t winguru/minecraft-spigot:latest \
+		-t winguru/minecraft-spigot-nightly:latest
 }
 
 run_spigot_server() {
-	docker run -it --name mc-spigot-${MC_VERSION} -e MC_VERSION=${MC_VERSION} winguru/minecraft-spigot
+	docker run -it --name mc-spigot-${MC_VERSION} -e MC_VERSION=${MC_VERSION} -p 25565:25565 winguru/minecraft-spigot
 }
 
-cleanup_spigot_server() {
+push_spigot_server() {
+	docker push winguru/minecraft-spigot:${MC_VERSION}
+	docker push winguru/minecraft-spigot:latest
+}
+
+cleanup_docker() {
 	docker rm mc-spigot-${MC_VERSION}
+	docker rm mc-spigot-builder
 }
 
-# Step 1: Create docker Java Development (JDK) build environment 
-make_builder
+case "$1" in
+prep)        echo "Step 1: Prepare docker Java Development (JDK) build environment"
+             make_builder
+             ;;
+compile)     echo "Step 2: Compile Spigot server Java Archive (JAR)"
+             run_builder
+             ;;
+build)       echo "Step 3: Build Spigot docker image"
+             make_spigot_server
+             ;;
+run)         echo "Step 4: Perform test-run validation of docker containter"
+             run_spigot_server	
+             ;;
+push)        echo "Step 5: Push Spigot docker image to docker-hub"
+             push_spigot_server
+             ;;
+clean)       echo "Step 6: Cleanup and remove docker containers and images"
+             cleanup_docker
+             ;;
+*)           echo "Usage: $0 {prep|compile|build|run|push|clean}"
+             exit 2
+             ;;
+esac
+exit 0
 
-# Step 2: Compile Spigot server Java Archive (JAR)
-run_builder
-
-# Step 3: Create docker Spigot Server
-make_spigot_server
-
-# Step 4: Verify Spigot Server runs poperly
-run_spigot_server
-
-# Step 5: Cleanup and remove docker container
-cleanup_spigot_server
